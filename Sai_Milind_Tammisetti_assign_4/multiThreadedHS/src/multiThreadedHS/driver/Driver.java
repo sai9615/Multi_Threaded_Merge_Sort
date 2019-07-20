77ffaea3d7f3d028ef59d874a6424e42a26eeaed
root <root@Sai-Milind.localdomain>
2019-07-20 00:35:32 -0400

    Start implementing multithreading in driver file

--- Sai_Milind_Tammisetti_assign_4/multiThreadedHS/src/multiThreadedHS/driver/Driver.java
+++ Sai_Milind_Tammisetti_assign_4/multiThreadedHS/src/multiThreadedHS/driver/Driver.java
@@ -37,12 +37,105 @@ import java.util.ArrayList;
 			System.out.println("The value of N can only be between 1 and 3");
 			System.exit(0);
 		}
+		
+		if( n == 1){
+			if ( (args.length != 4) || args[0].equals("${arg0}") || args[1].equals("${arg1}") || args[2].equals("${arg2}")|| args[3].equals("${arg3}")) {
 
-	    if ( (args.length != n+3) || args[0].equals("${arg0}") || args[1].equals("${arg1}") || args[2].equals("${arg2}")|| args[3].equals("${arg3}")|| args[4].equals("${arg4}")|| args[5].equals("${arg5}")) {
-		    
-		    System.err.println("Error: Incorrect number of arguments. Program accepts"+ (n+3) +" argumnets.");
-		    System.exit(0);
-	    } // end of if
+				System.err.println("Error: Incorrect number of arguments. Program accepts "+ (n+3) +" argumnets.");
+				System.exit(0);
+			}
+			inputf1 = args[1];
+			output = args[2];
+			Debug = args[3];
+			MyLogger obj = new MyLogger();
+
+			int dbglvl = Integer.parseInt(Debug);
+			if(dbglvl <0 || dbglvl > 4){
+				System.out.println("Enter proper dbg option from 1 to 4 \n");
+				System.out.println("0:RELEASE, 1:SHOW ERROR IF ENCOUNTERED, 2:SHOW RESULT TO STDOUT 3:PRINT WHEN CHANGE IN STATE 4:PRINT WHENEVER CONSTRUCTOR IS CALLED \n");
+				System.exit(0);
+			}
+			else {
+
+				obj.setDebugValue(dbglvl);
+				System.out.println("debug level set to "+dbglvl);
+			}
+			Results res = new Results(output);
+
+			ThreadWorker threadWorker = new  ThreadWorker(inputf1, res);
+			threadWorker.start();
+			res.writeResults();
+			res.closeMyFile();
+
+		}	else if (n==2){
+				if ( (args.length != 5) || args[0].equals("${arg0}") || args[1].equals("${arg1}") || args[2].equals("${arg2}")|| args[3].equals("${arg3}") || args[4].equals("${arg4}")) {
+
+					System.err.println("Error: Incorrect number of arguments. Program accepts"+ (n+3) +" argumnets.");
+					System.exit(0);
+			}
+			inputf1 = args[1];
+			inputf2 = args[2];
+			output = args[3];
+			Debug = args[4];
+			MyLogger obj = new MyLogger();
+
+			int dbglvl = Integer.parseInt(Debug);
+			if(dbglvl <0 || dbglvl > 4){
+				System.out.println("Enter proper dbg option from 1 to 4 \n");
+				System.out.println("0:RELEASE, 1:SHOW ERROR IF ENCOUNTERED, 2:SHOW RESULT TO STDOUT 3:PRINT WHEN CHANGE IN STATE 4:PRINT WHENEVER CONSTRUCTOR IS CALLED \n");
+				System.exit(0);
+			}
+			else {
+
+				obj.setDebugValue(dbglvl);
+				System.out.println("debug level set to "+dbglvl);
+			}
+
+			Results res = new Results(output);
+
+			ThreadWorker threadWorker1 = new  ThreadWorker(inputf1, res);
+			ThreadWorker threadWorker2 = new  ThreadWorker(inputf2, res);
+			threadWorker1.start();
+			threadWorker2.start();
+			res.writeResults();
+			res.closeMyFile();
+
+		} else if (n==3){
+			if ( (args.length !=6) || args[0].equals("${arg0}") || args[1].equals("${arg1}") || args[2].equals("${arg2}")|| args[3].equals("${arg3}") || args[4].equals("${arg4}")|| args[5].equals("${arg5}")) {
+
+				System.err.println("Error: Incorrect number of arguments. Program accepts"+ (n+3) +" argumnets.");
+				System.exit(0);
+			}
+			inputf1 = args[1];
+			inputf2 = args[2];
+			inputf3 = args[3];
+			output = args[4];
+			Debug = args[5];
+			MyLogger obj = new MyLogger();
+
+			int dbglvl = Integer.parseInt(Debug);
+			if(dbglvl <0 || dbglvl > 4){
+				System.out.println("Enter proper dbg option from 1 to 4 \n");
+				System.out.println("0:RELEASE, 1:SHOW ERROR IF ENCOUNTERED, 2:SHOW RESULT TO STDOUT 3:PRINT WHEN CHANGE IN STATE 4:PRINT WHENEVER CONSTRUCTOR IS CALLED \n");
+				System.exit(0);
+			}
+			else {
+
+				obj.setDebugValue(dbglvl);
+				System.out.println("debug level set to "+dbglvl);
+			}
+			Results res = new Results(output);
+
+			ThreadWorker threadWorker1 = new  ThreadWorker(inputf1, res);
+			ThreadWorker threadWorker2 = new  ThreadWorker(inputf2, res);
+			ThreadWorker threadWorker3 = new  ThreadWorker(inputf3 , res);
+			threadWorker1.start();
+			threadWorker2.start();
+			threadWorker3.start();
+			res.writeResults();
+			res.closeMyFile();
+		}
+
 		
 	}  // end public static void main
     }  // end public class Driver
